b435298c32eeb21b6115380f217b436f6ded4a05
Devkarthik <175382935+DevkarthikSuresh@users.noreply.github.com>
2024-10-09 15:27:28 +0530

    Update age_exception.cpp

--- age_exception.cpp
+++ age_exception.cpp
@@ -4,24 +4,39 @@ PRN:23070123045
 Aim: Exception to check for age below 18.
 #include <iostream>
   */
+#include <iostream>
 using namespace std;
+
 int main() {
-float age;
+    float age;
 
     cout << "Enter your age: ";
-    cin >>age;
-    try{
-        if(age<18){
-            throw age;}
-        else{
-            cout << "Accepted";
+    cin >> age;
+
+    try {
+        if (age < 18) {
+            throw age; 
+        } 
+        else if (age < 0) { 
+            throw age; 
+        } 
+        else {
+            cout << "Accepted" << endl;
+            
         }
     }
-catch(float age){
-    cout<<"\n You are below 18"<<endl;
-}
+    catch (float age) {
+        if (age < 0) {
+            cout << "\nInvalid age"<<endl;
+        } 
+        else {
+            cout << "\nYou are below 18" << endl; 
+        }
+    }
+
     return 0;
 }
+
 /*Output
 Enter your age: 10
 
